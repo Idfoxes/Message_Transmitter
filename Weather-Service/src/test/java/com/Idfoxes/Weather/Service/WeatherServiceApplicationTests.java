2d3a661f9c9bf0922b6715e11faa0fca5fbb6f15
Daniil Tishin <danatiska5@gmail.com>
2024-09-16 11:05:18 +0300

    start project

---
+++ Weather-Service/src/test/java/com/Idfoxes/Weather/Service/WeatherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Idfoxes.Weather.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
